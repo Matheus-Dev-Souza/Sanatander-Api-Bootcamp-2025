149136f9a7683e33c64d09c1d08f7d6b5ab0d88c
Mateus <goncalvesouza1996@gmail.com>
2025-07-04 15:52:04 -0300

    delete: pasta sudoku

--- sudoku/src/main/java/sudoku_desafio_dio/sudoku/service/EventEnum.java
+++
@@ -1,7 +0,0 @@
-package sudoku_desafio_dio.sudoku.service;
-
-public enum EventEnum {
-
-    CLEAR_SPACE
-
-}
--- sudoku/src/main/java/sudoku_desafio_dio/sudoku/service/EventListener.java
+++
@@ -1,7 +0,0 @@
-package sudoku_desafio_dio.sudoku.service;
-
-public interface EventListener {
-
-    void update(final EventEnum eventType);
-
-}
